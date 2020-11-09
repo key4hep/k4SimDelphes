85b9b8422dc78e390609e0e9f81ef6e0ee42c0d5
Valentin Volkl <valentin.volkl@cern.ch>
2020-11-09 11:17:57 +0100

    [cmake] better pythia8 handling

--- cmake/FindPythia8.cmake
+++ cmake/FindPythia8.cmake
@@ -1,58 +1,54 @@
-# Find the Pythia8 includes and library.
+# - Locate pythia6 library
+# Defines:
 #
-# This module defines
-# PYTHIA8_INCLUDE_DIR   where to locate Pythia.h file
-# PYTHIA8_LIBRARY       where to find the libpythia8 library
-# PYTHIA8_<lib>_LIBRARY Addicional libraries
-# PYTHIA8_LIBRARIES     (not cached) the libraries to link against to use Pythia8
-# PYTHIA8_FOUND         if false, you cannot build anything that requires Pythia8
-# PYTHIA8_VERSION       version of Pythia8 if found
+#  PYTHIA8_FOUND
+#  PYTHIA8_VERSION
+#  PYTHIA8_INCLUDE_DIR
+#  PYTHIA8_INCLUDE_DIRS (not cached)
+#  PYTHIA8_LIBRARY
+#  PYTHIA8_hepmcinterface_LIBRARY
+#  PYTHIA8_lhapdfdummy_LIBRARY
+#  PYTHIA8_LIBRARIES (not cached) : for PYTHIA8_VERSION < 200 includes 3 libraries above; not to be used if lhapdf is used
 
-set(_pythia8dirs
-    ${PYTHIA8}
-    $ENV{PYTHIA8}
-    ${PYTHIA8_DIR}
-    $ENV{PYTHIA8_DIR}
-    /usr
-    /opt/pythia8)
 
-find_path(PYTHIA8_INCLUDE_DIR
-          NAMES Pythia8/Pythia.h
-          HINTS ${_pythia8dirs}
-          PATH_SUFFIXES include include/Pythia8 include/pythia8
-          DOC "Specify the directory containing Pythia.h.")
+find_path(PYTHIA8_INCLUDE_DIR Pythia.h Pythia8/Pythia.h
+          HINTS $ENV{PYTHIA8}/include ${PYTHIA8_ROOT_DIR}/include)
+find_path(PYTHIA8_XML_DIR Version.xml
+          HINTS $ENV{PYTHIA8}/xmldoc ${PYTHIA8_ROOT_DIR}/xmldoc $ENV{PYTHIA8}/share/Pythia8/xmldoc ${PYTHIA8_ROOT_DIR}/share/Pythia8/xmldoc)
 
-find_library(PYTHIA8_LIBRARY
-             NAMES pythia8 Pythia8
-             HINTS ${_pythia8dirs}
-             PATH_SUFFIXES lib
-             DOC "Specify the Pythia8 library here.")
+message(STATUS "xml path: ${PYTHIA8_XML_DIR}")
 
-find_library(PYTHIA8_hepmcinterface_LIBRARY
-             NAMES hepmcinterface pythia8tohepmc
-             HINTS ${_pythia8dirs}
-             PATH_SUFFIXES lib)
+#file(READ ${PYTHIA8_INCLUDE_DIR}/../xmldoc/Version.xml versionstr)
+file(READ ${PYTHIA8_XML_DIR}/Version.xml versionstr)
+string(REGEX REPLACE ".*Pythia:versionNumber.*default.*[0-9][.]([0-9]+).*" "\\1" PYTHIA8_VERSION "${versionstr}")
 
-find_library(PYTHIA8_lhapdfdummy_LIBRARY
-             NAMES lhapdfdummy
-             HINTS ${_pythia8dirs}
-             PATH_SUFFIXES lib)
+message(STATUS "pythia8 version extracted: ${PYTHIA8_VERSION}")
 
-foreach(_lib PYTHIA8_LIBRARY PYTHIA8_hepmcinterface_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
-  if(${_lib})
-    set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARIES} ${${_lib}})
-  endif()
-endforeach()
+find_library(PYTHIA8_LIBRARY NAMES pythia8 Pythia8
+             HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+
+if(PYTHIA8_VERSION VERSION_LESS 200)
+  find_library(PYTHIA8_hepmcinterface_LIBRARY NAMES hepmcinterface pythia8tohepmc
+               HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+
+  find_library(PYTHIA8_lhapdfdummy_LIBRARY NAMES lhapdfdummy
+               HINTS $ENV{PYTHIA8_ROOT_DIR}/lib ${PYTHIA8_ROOT_DIR}/lib)
+
+  set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY} ${PYTHIA8_hepmcinterface_LIBRARY} ${PYTHIA8_lhapdfdummy_LIBRARY})
+else()
+  set(PYTHIA8_LIBRARIES ${PYTHIA8_LIBRARY})
+endif()
+
 set(PYTHIA8_INCLUDE_DIRS ${PYTHIA8_INCLUDE_DIR} ${PYTHIA8_INCLUDE_DIR}/Pythia8 )
-
-find_path(PYTHIA8_DATA 
-          NAMES MainProgramSettings.xml
-          HINTS ${_pythia8dirs}
-          PATH_SUFFIXES xmldoc)
 
 # handle the QUIETLY and REQUIRED arguments and set PYTHIA8_FOUND to TRUE if
 # all listed variables are TRUE
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY)
-mark_as_advanced(PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_hepmcinterface_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
+INCLUDE(FindPackageHandleStandardArgs)
+if(PYTHIA8_VERSION VERSION_LESS 200)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_hepmcinterface_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY)
+  mark_as_advanced(PYTHIA8_FOUND PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_hepmcinterface_LIBRARY PYTHIA8_lhapdfdummy_LIBRARY PYTHIA8_VERSION_CHECK)
+else()
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pythia8 DEFAULT_MSG PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY)
+  mark_as_advanced(PYTHIA8_FOUND PYTHIA8_INCLUDE_DIR PYTHIA8_LIBRARY PYTHIA8_VERSION_CHECK)
+endif()
