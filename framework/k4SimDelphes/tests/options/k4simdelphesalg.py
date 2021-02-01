e797560cda7bd5f6c7ef83ca3bf186ea5d458de2
Valentin Volkl <valentin.volkl@cern.ch>
2021-02-01 12:34:07 +0100

    [wip] test update

--- framework/k4SimDelphes/tests/options/k4simdelphesalg.py
+++ framework/k4SimDelphes/tests/options/k4simdelphesalg.py
@@ -1,19 +1,22 @@
 from Gaudi.Configuration import *
 
-from Configurables import k4DataSvc
-podioevent = k4DataSvc("EventDataSvc")
-
-from Configurables import CreateExampleEventData
-producer = CreateExampleEventData()
-
-from Configurables import k4SimDelphesAlg
-delphesalg = k4SimDelphesAlg()
-delphesalg.GenParticles.Path = "MCParticles"
-
-
+PluginDebugLevel=1
 from Configurables import ApplicationMgr
 app = ApplicationMgr()
 app.EvtMax = 100
-app.ExtSvc = [podioevent]
 app.EvtSel = "NONE"
-app.TopAlg = [producer, delphesalg]
+
+from Configurables import k4DataSvc
+podioevent = k4DataSvc("EventDataSvc")
+app.ExtSvc += [podioevent]
+
+from Configurables import ConstPtParticleGun
+gun = ConstPtParticleGun()
+app.TopAlg = [gun]
+
+#from Configurables import k4SimDelphesAlg
+#delphesalg = k4SimDelphesAlg()
+#delphesalg.GenParticles.Path = "MCParticles"
+
+
+
