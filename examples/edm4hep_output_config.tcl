bff2f15e732411d3e2eea7f15cba408bf1aa8e5c
clementhelsens <clement.helsens@cern.ch>
2021-04-27 17:14:28 +0200

    remove collections

--- examples/edm4hep_output_config.tcl
+++ examples/edm4hep_output_config.tcl
@@ -1,12 +1,9 @@
 module EDM4HepOutput EDM4HepOutput {
     add ReconstructedParticleCollections EFlowTrack EFlowPhoton EFlowNeutralHadron
     add GenParticleCollections           Particle
-    add JetCollections                   Jet
     add MuonCollections                  Muon
     add ElectronCollections              Electron
     add PhotonCollections                Photon
-    add MissingETCollections             MissingET
-    add ScalarHTCollections              ScalarHT
     set RecoParticleCollectionName       ReconstructedParticles
     set MCRecoAssociationCollectionName  MCRecoAssociations
  }
