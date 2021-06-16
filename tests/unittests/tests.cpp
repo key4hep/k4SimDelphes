bfde769e7cb44b53dafa1309ba7bef6623eed826
Thomas Madlener <thomas.madlener@desy.de>
2021-06-16 17:41:44 +0200

    Fix unittests by providing a readable delphes card

--- tests/unittests/tests.cpp
+++ tests/unittests/tests.cpp
@@ -6,12 +6,15 @@
 
 #include "k4SimDelphes/DelphesEDM4HepConverter.h"
 #include "k4SimDelphes/DelphesEDM4HepOutputConfiguration.h"
+
+#include "test_utils.h"
+
 using namespace k4SimDelphes;
 
 TEST_CASE( "k4SimDelphes Converter Tests", "[converter]" ) {
 
     SECTION("DelphesEDM4HepConverter ctor");
-    DelphesEDM4HepConverter conv = DelphesEDM4HepConverter("data/delphes_card_IDEA.tcl");
+    DelphesEDM4HepConverter conv = DelphesEDM4HepConverter(getDelphesCard());
 
     SECTION("DelphesEDM4HepConverter getCollections");
     auto coll = conv.getCollections();
